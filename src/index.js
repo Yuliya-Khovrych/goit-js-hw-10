8ddf860fa2cb7775d476c277d10f6b68c81a6041
Yuliya-Khovrych <ulia.voitishenko@gmail.com>
2022-10-15 13:51:41 +0300

    task1

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ function renderCountryList(countries) {
     .map(({ name, flags }) => {
       return `
           <li class="country-list__item">
-              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 30px height = 30px>
+              <img class="country-list__flag" src="${flags.svg}" alt="Flag of ${name.official}" width = 50px height = auto>
               <h2 class="country-list__name">${name.official}</h2>
           </li>
           `;
